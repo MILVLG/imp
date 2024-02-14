202df57995acb76f008c8d480aa9d63cb083b66d
Zhenwei <964730078@qq.com>
2024-02-14 16:00:43 +0800

    Update finetune_lora.sh

--- scripts/finetune_lora.sh
+++ scripts/finetune_lora.sh
@@ -9,7 +9,7 @@ export IMP_SILIENT_OTHERS=true
 # if not use all GPUs 
 # deepspeed --include localhost:0,1,2,3 --master_port 29600
 
-deepspeed llava/train/train_mem.py \
+deepspeed imp_llava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path microsoft/phi-2 \
