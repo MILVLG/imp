bfdeae258b75423951c9e5c08879fd799c98d401
ParadoxZW <964730078@qq.com>
2024-02-26 16:52:40 +0800

    fix some bugs

--- imp_llava/train/llava_trainer.py
+++ imp_llava/train/llava_trainer.py
@@ -84,11 +84,8 @@ def get_modality_length_grouped_indices(lengths, batch_size, world_size, generat
     megabatch_indices = torch.randperm(len(megabatches), generator=generator)
     megabatches = [megabatches[i] for i in megabatch_indices]
 
-    if len(additional_batch) > megabatch_size:
-        megabatches.append(additional_batch[:megabatch_size])
-        megabatches.append(additional_batch[megabatch_size:])
-    elif len(additional_batch) > 0:
-        megabatches.append(additional_batch)
+    if len(additional_batch) > 0:
+        megabatches.append(sorted(additional_batch, key=lambda i: abs(lengths[i]), reverse=True))
 
     return [i for megabatch in megabatches for i in megabatch]
 
