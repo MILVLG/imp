0d243ac795de47e228258e1bdca82f5036d8240d
Leo Zhang <1095412419@qq.com>
2024-03-06 18:48:56 +0800

    Fix bug in train.py
    Fix bug when encountering an int value such as '12'

--- imp_llava/train/train.py
+++ imp_llava/train/train.py
@@ -679,7 +679,7 @@ class LazySupervisedDataset(Dataset):
     def modality_lengths(self):
         length_list = []
         for sample in self.list_data_dict:
-            cur_len = sum(len(conv['value'].split()) for conv in sample['conversations'])
+            cur_len = sum(len(str(conv['value']).split()) for conv in sample['conversations'])
             cur_len = (cur_len + 729) if 'image' in sample else -cur_len
             length_list.append(cur_len)
         return length_list
